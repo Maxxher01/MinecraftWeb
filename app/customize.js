4ef490fe52d78f1216a559f54d80a7f5cd8a104a
Maxxher01 <terrarpgminecraftserver@gmail.com>
2024-10-22 20:39:44 -0500

    Update customize.js

--- app/customize.js
+++ app/customize.js
@@ -1,15 +1,15 @@
 const customize = {
-        Servername: "EventyrMC",
-        Yourserverip: 'play.eventyr.lat',
-        Webtitle: "EventyrMC - Servidor Survival",
-        Webdesc: "EventyrMC: El mejor servidor de Minecraft en Español con Towny y una comunidad amigable. Únete a nuestra experiencia de juego única y descubre emocionantes desafios.",
-        Weburl: "https://eventyr.lat", //DON'T ADD SLASH AND THE END OF THE DOMAIN
-        Discord: "https://discord.eventyr.lat/",
-        Store: "https://tienda.eventyr.lat/",
-        Webmap: "https://mapa.eventyr.lat/",
-        Ownernick: "emoticones",
+        Servername: "TerraRPG",
+        Yourserverip: 'mc.terrarpg.net',
+        Webtitle: "TerraRPG - Servidor Survival",
+        Webdesc: "TerraRPG: El mejor servidor de Minecraft en Español estilo RPG  y con una comunidad amigable. Únete a nuestra experiencia de juego única y descubre emocionantes desafios.",
+        Weburl: "https://terrarpg.net", //DON'T ADD SLASH AND THE END OF THE DOMAIN
+        Discord: "https://discord.terrarpg.net/",
+        Store: "https://tienda.terrarpg.net/",
+        Webmap: "#",
+        Ownernick: "maximiliano",
         Serverlocale: "es_ES",
-        Servertwitter: "@eventyrmc",
+        Servertwitter: "#",
         Keywords: ['Servidor Minecraft', 'Jugar Minecraft multijugador', 'Servidores Minecraft mejores', 'Comunidad Minecraft', 'Servidor supervivencia Minecraft', 'Servidor PvP Minecraft', 'Servidor rol Minecraft', 'Mapas Minecraft personalizados', 'Servidores Minecraft lista', 'Mejores servidores Minecraft', 'Modos juego Minecraft', 'Minecraft plugins', 'Minecraft mods', 'IP servidor Minecraft', 'Unirse servidor Minecraft', 'Minecraft online', 'Reseñas servidores Minecraft', 'Foro comunidad Minecraft']
     };
 
